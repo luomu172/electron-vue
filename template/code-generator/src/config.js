3338e1576f8c57925481a3f483382ebdd9e46bcc
xiaole <xiaolejt@xiao100.com>
2020-07-25 15:13:17 +0800

    fix: 解决模板报错

--- template/code-generator/src/config.js
+++ template/code-generator/src/config.js
@@ -5,13 +5,10 @@
  * @date 2018/8/4 0:49
  */
 /**
- * @description
- * @type {{
  * swaggerUrl: string,  swagger地址
  * modules: string[],   模块名称
  * template: string,    模版文件
  * dist: string         dist路径,相对于index.js的路径
- * }}
  */
 const config = {
   swaggerUrl: 'http://teaching-swagger-service-test.c34a2fa211afc47939399469144eee06c.cn-shenzhen.alicontainer.com/load-api-docs?host=172.20.1.10&port=8080&group=teaching-question-query-service&domain=test-tapi-question-query.xiaojiaoyu100.com',
