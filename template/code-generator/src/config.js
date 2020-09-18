40a32f9fed449072dd7f2ecb8f88ba3903e976e6
xiaole <xiaolejt@xiao100.com>
2020-09-18 17:10:42 +0800

    feat: 更新接口生成工具的配置

--- template/code-generator/src/config.js
+++ template/code-generator/src/config.js
@@ -5,15 +5,27 @@
  * @date 2018/8/4 0:49
  */
 /**
+ * @description
+ * @type {{
  * swaggerUrl: string,  swagger地址
  * modules: string[],   模块名称
  * template: string,    模版文件
  * dist: string         dist路径,相对于index.js的路径
+ * }}
  */
+const swagger = {
+  url: '',
+  host: '',
+  port: '',
+  group: '',
+  domain: ''
+};
+const swaggerUrl = `${swagger.url}?host=${swagger.host}&port=${swagger.port}&group=${swagger.group}&domain=${swagger.domain}`;
+console.log(swaggerUrl);
 const config = {
-  swaggerUrl: '',
+  swaggerUrl,
   template: './teaching-service-tmp',
-  dist: '../../src/renderer/js/api/'
+  dist: '../../src/app/common/service/'
 };
 
 module.exports = config;
